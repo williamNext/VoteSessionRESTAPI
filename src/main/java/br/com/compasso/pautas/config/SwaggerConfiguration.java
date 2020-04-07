a05a3385ddec68173dfcd2e7536aceee47d3af72
William Andrei Nascimento Silveira <william.silveira@pampa.compasso>
2020-04-07 15:25:07 -0300

    renomenado pacote

--- src/main/java/br/com/compasso/pautas/config/SwaggerConfiguration.java
+++ src/main/java/br/com/compasso/pautas/config/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package br.com.compasso.pautas.config.securoty;
+package br.com.compasso.pautas.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
